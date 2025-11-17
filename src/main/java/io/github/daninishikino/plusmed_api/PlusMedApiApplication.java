e3747c2228baf3da3e6f02139c9a342887a4eb30
Dani <nimguem1453@gmail.com>
2025-11-17 18:06:50 -0300

    Feature: API PlusMed init.

---
+++ src/main/java/io/github/daninishikino/plusmed_api/PlusMedApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.daninishikino.plusmed_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusMedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusMedApiApplication.class, args);
+    }
+
+}
